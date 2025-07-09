1ed5872a5a1ba6efd94191f83b88fe83cf0e105a
Mihaylov-Ivan <im566@bath.ac.uk>
2025-07-09 18:34:06 +0300

    styling

--- src/app/components/subpage-selector-btn.tsx
+++ src/app/components/subpage-selector-btn.tsx
@@ -16,8 +16,8 @@ export default function SubpageSelectorBtn({
   return (
     <button
       onClick={onClick}
-      className={`flex-1 sm:flex-none px-4 py-2 text-sm sm:text-base font-medium rounded-lg transition-colors focus:outline-none border border-[var(--surface-3)] hover:bg-[var(--surface-3)]
-        ${active ? "bg-[var(--surface-2)]" : "bg-[var(--surface-1)] text-[var(--on-surface-2)]"}`}
+      className={`flex-1 w-full sm:flex-none px-4 py-2 text-sm sm:text-base font-medium transition-colors focus:outline-none border-b-3 border-[var(--surface-2)] hover:border-[var(--surface-3)]
+        ${active ? "bg-[var(--surface-2)]" : "bg-[var(--surface-1)] text-[var(--on-surface-2)] opacity-75 hover:opacity-100"}`}
     >
       {label}
     </button>
